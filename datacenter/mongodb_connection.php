fb98995c1a52197dd2b180df5ab8221df001f027
namth <namth.pass@gmail.com>
2024-08-27 22:32:59 +0700

    Export custom job table to MongoDB with updated country field

--- datacenter/mongodb_connection.php
+++ datacenter/mongodb_connection.php
@@ -1165,7 +1165,7 @@ function setup_page_number(){
     # connect to database
     global $wpdb;
     $limit = 20;
-    $aslTable = $wpdb->prefix . 'asljob';
+    $aslTable = $wpdb->prefix . 'asljobcountry';
 
     $count_sql  = "SELECT COUNT(*) FROM $aslTable";
     $rowcount   = $wpdb->get_var($count_sql);
@@ -1183,13 +1183,13 @@ function update_data_with_page(){
     # connect to database
     global $wpdb;
 
-    $aslTable = $wpdb->prefix . 'asljob';
+    $aslTable = $wpdb->prefix . 'asljobcountry';
     
     $limit = 20;
     $page = $_POST['page'];
     $total_page = $_POST['total_page'];
-    // $prev_jobid = $_POST['prev_jobid'];
-    // $tmp_countries = explode(', ', $_POST['countries']);
+    $prev_jobid = $_POST['prev_jobid'];
+    $tmp_countries = explode(', ', $_POST['countries']);
     
     $offset  = ($page - 1) * $limit;
     $logid = "";
@@ -1204,7 +1204,7 @@ function update_data_with_page(){
 
     if (!empty($data)) {
         foreach ($data as $key => $value) {
-            $jobid = $value['jobid'];
+            /* $jobid = $value['jobid'];
             
             # update data to custom field by jobid
             $currency       = $value['currency'];
@@ -1224,26 +1224,28 @@ function update_data_with_page(){
             update_field('field_60afae64cfd69', $total_cost, $jobid);
             update_field('field_60afaeb8cfd6a', $advance_money, $jobid);
             update_field('field_60afaf50cfd6b', $debt, $jobid);
-            update_field('field_60afafbccfd6c', $currency_out, $jobid);
+            update_field('field_60afafbccfd6c', $currency_out, $jobid); */
             
-            // $customerid = $value['customerid'];
-            // $country  = $value['country'];
-
+            $jobid = $value['jobid'];
+            $country  = $value['country'];
+            
             # if not same jobid and prev_jobid and it is the last item of foreach, update country to job
-            /* if ($prev_jobid != $jobid) {
+            if ($prev_jobid != $jobid) {
                 # update country to job
                 $countries = implode(", ", $tmp_countries);
                 update_field('field_6099f6bb87256', $countries, $prev_jobid);
-
+                
                 # add country to array
                 $tmp_countries = array($country);
                 $prev_jobid = $jobid;
             } else {
                 $tmp_countries[] = $country;
-            } */
-
-            # update country to customer
-            // update_field('field_6037200ec98cc', $country, $customerid);
+            }
+            
+            /* # update country to customer
+            $country  = $value['country'];
+            $customerid = $value['customerid'];
+            update_field('field_6037200ec98cc', $country, $customerid); */
         }
     }
 
@@ -1251,8 +1253,8 @@ function update_data_with_page(){
         'status'        => 'success',
         'current_page'  => ++$page,
         'total_page'    => $total_page,
-        // 'prev_jobid'    => $prev_jobid,
-        // 'countries'     => implode(", ", $tmp_countries),
+        'prev_jobid'    => $prev_jobid,
+        'countries'     => implode(", ", $tmp_countries),
         'result'        => $logid
     ]);
 
